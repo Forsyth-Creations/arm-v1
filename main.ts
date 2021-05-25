b27217e24a4b7d7cdcabbbd5dd4b91ca4e8b46ec
Forsyth Creations <robert.h.forsyth@gmail.com>
2021-05-25 09:37:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@ basic.pause(1000)
 led.enable(false)
 makerbit.setLedPins(makerbit.level(PinLevel.Low))
 basic.forever(function () {
-	
+    PCA9685.setServoPosition(PCA9685.ServoNum.Servo1, Math.map(90, 0, 270, 0, 180), 64)
 })
